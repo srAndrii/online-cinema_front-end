166d07d2e8ce046dad4855090caee9afa9c94a2a
srAndrii <55akif5548@gmail.com>
2022-08-13 19:44:08 +0300

    Development settings

--- next.config.js
+++ next.config.js
@@ -2,6 +2,24 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  poweredByHeader: false,
+  env: {
+    APP_URL: process.env.REACT_APP_URL,
+    APP_ENV: process.env.REACT_APP_ENV,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `http://localhost:4200/api/:path*`,
+      },
+      {
+        source: '/uploads/:path*',
+        destination: `http://localhost:4200/uploads/:path*`,
+      },
+    ]
+  },
+
 }
 
 module.exports = nextConfig
